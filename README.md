5203c96bd6439bb6f04aadef7291fceb51d744c6
rodrigomirco <rodrigomirco@hotmail.com>
2019-01-22 11:16:39 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# vaolo-json
---
+++ dev/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+args="-p 80"
+
+file=/data/db.json
+if [ -f $file ]; then
+    echo "Found db.json, trying to open"
+    args="$args db.json"
+fi
+
+json-server $args
